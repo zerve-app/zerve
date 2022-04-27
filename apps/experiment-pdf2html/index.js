7981afef39e080be68b74d2385ac1e5e9b8c53df
Eric Vicenti <eric@vicenti.net>
2022-04-26 22:34:23 -0700

    clean up experiment + old pkg cache

--- apps/experiment-pdf2html/index.js
+++
@@ -1,9 +0,0 @@
-const pdf2html = require("pdf2html");
-
-pdf2html.html("protocol.pdf", (err, html) => {
-  if (err) {
-    console.error("Conversion error: " + err);
-  } else {
-    console.log(html);
-  }
-});
