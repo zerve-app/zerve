8f1991aa2d90396b2dc7220f7252fd26c54e00a8
Eric Vicenti <eric@vicenti.net>
2022-05-05 12:37:39 -0700

    attempt rnx-kit install in zoo-mobile, fails

--- apps/zoo-mobile/metro.config.js
+++ apps/zoo-mobile/metro.config.js
@@ -1,19 +1,29 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-/**
- * @type {import('expo/metro-config')}
- */
-const { getDefaultConfig } = require('expo/metro-config')
-const path = require('path')
+// /**
+//  * @type {import('expo/metro-config')}
+//  */
+// const { getDefaultConfig } = require("expo/metro-config");
+// const path = require("path");
 
-const projectRoot = __dirname
-const workspaceRoot = path.resolve(__dirname, '../..')
+// const projectRoot = __dirname;
+// const workspaceRoot = path.resolve(__dirname, "../..");
 
-const config = getDefaultConfig(projectRoot)
+// const config = getDefaultConfig(projectRoot);
 
-config.watchFolders = [workspaceRoot]
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
-]
+// config.watchFolders = [workspaceRoot];
+// config.resolver.nodeModulesPaths = [
+//   path.resolve(projectRoot, "node_modules"),
+//   path.resolve(workspaceRoot, "node_modules"),
+// ];
 
-module.exports = config
+// module.exports = config;
+
+const { makeMetroConfig } = require("@rnx-kit/metro-config");
+const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
+
+module.exports = makeMetroConfig({
+  projectRoot: __dirname,
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+  },
+});
