f47ff76c3089addd9ea1688ccbd5f28358dcded1
Eric Vicenti <eric@vicenti.net>
2022-05-24 09:09:58 -0700

    minor aardvark2

--- config/UpdateAardvark.sh
+++ config/UpdateAardvark.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-# # Steps to run this script
-# cd /root/zerve.git
-# git fetch
-# cd /root
-# git clone zerve.git aardvark-build
-# cd aardvark-build
-# ./config/UpdateAardvark.sh
+: '
+Steps to run this script:
 
+cd /root/zerve.git
+git fetch
+cd /root
+git clone zerve.git aardvark-build
+cd aardvark-build
+./config/UpdateAardvark.sh
+
+'
 
 yarn
 
@@ -20,13 +23,15 @@ yarn workspace aardvark-server build
 systemctl stop AardvarkWeb.service
 systemctl stop AardvarkServer.service
 
+cd /root
 mv aardvark aardvark-old
-
 mv aardvark-build aardvark
-
 cp secrets.json aardvark/secrets.json
 
 systemctl start AardvarkWeb.service
 systemctl start AardvarkServer.service
 
-#rm -rf aardvark-old
+systemctl status AardvarkWeb.service
+systemctl status AardvarkServer.service
+
+rm -rf aardvark-old
