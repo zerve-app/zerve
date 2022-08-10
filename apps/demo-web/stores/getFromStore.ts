6ed1d5ae7662385725dc32185d53867270f3bf1b
Eric Vicenti <eric@vicenti.net>
2022-08-10 23:11:30 +0200

    🔥 delete experiments/demos/junk

--- apps/demo-web/stores/getFromStore.ts
+++
@@ -1,9 +0,0 @@
-import { getZ } from "@zerve/client/ServerCalls";
-
-export async function getFromStore(fileKey: string) {
-  return await getZ({ url: "http://localhost:3888", key: "" }, [
-    "Store",
-    "State",
-    fileKey,
-  ]);
-}
