a4ffb64f0094046058955acd7838f19f1dcdf8ef
Eric Vicenti <eric@vicenti.net>
2022-08-10 02:19:50 +0200

    🐈‍⬛ revert commentedout

--- packages/zoo/app/useCachedResources.ts
+++ packages/zoo/app/useCachedResources.ts
@@ -7,27 +7,27 @@ export default function useCachedResources() {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
 
   // Load any resources or data that we need prior to rendering the app
-  // useEffect(() => {
-  //   async function loadResourcesAndDataAsync() {
-  //     try {
-  //       SplashScreen.preventAutoHideAsync();
+  useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHideAsync();
 
-  //       // Load fonts
-  //       await Font.loadAsync({
-  //         ...FontAwesome.font,
-  //         "space-mono": require("../assets/fonts/SpaceMono-Regular.ttf"),
-  //       });
-  //     } catch (e) {
-  //       // We might want to provide this error information to an error reporting service
-  //       console.warn(e);
-  //     } finally {
-  //       setLoadingComplete(true);
-  //       SplashScreen.hideAsync();
-  //     }
-  //   }
+        // Load fonts
+        await Font.loadAsync({
+          ...FontAwesome.font,
+          "space-mono": require("../assets/fonts/SpaceMono-Regular.ttf"),
+        });
+      } catch (e) {
+        // We might want to provide this error information to an error reporting service
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hideAsync();
+      }
+    }
 
-  //   loadResourcesAndDataAsync();
-  // }, []);
+    loadResourcesAndDataAsync();
+  }, []);
 
-  return true;
+  return isLoadingComplete;
 }
