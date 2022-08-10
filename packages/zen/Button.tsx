ba0e538996244a9d82a2754f7a2bd0df16be43f8
Eric Vicenti <eric@vicenti.net>
2022-08-11 00:25:08 +0200

    🐿 tamagui basic attempt

--- packages/zen/Button.tsx
+++ packages/zen/Button.tsx
@@ -12,6 +12,7 @@ import { useColors } from "./useColors";
 import { Spinner } from "./Spinner";
 import { ThemedText } from "./Themed";
 import { showErrorToast } from "./Toast";
+import { Button as TButton } from "tamagui";
 import { smallShadow } from "./Style";
 
 export function IconButton({
@@ -60,7 +61,7 @@ export type ButtonProps = {
   title: string;
   primary?: boolean;
   danger?: boolean;
-  onPress: null | (() => void);
+  onPress?: undefined | (() => void);
   onLongPress?: () => void;
   style?: StyleProp<ViewStyle>;
   border?: number;
@@ -82,78 +83,7 @@ export function Button({
   disabled,
   textAlign = "center",
 }: ButtonProps) {
-  const colors = useColors();
-  const pressHeight = useSharedValue(0.5); // 0.5 = inactive, 1 = hover/active, 0 = pressed
-
-  const color = danger
-    ? colors.dangerText
-    : primary
-    ? colors.tint
-    : colors.text;
-  const borderWidth = border !== undefined ? border : primary || danger ? 4 : 0;
-  const containerStyle = useAnimatedStyle(() => ({
-    backgroundColor: colors.background,
-    borderWidth,
-    borderColor: `${color}88`,
-    borderRadius: Layout.borderRadius,
-    padding: small ? 8 : 12,
-    paddingHorizontal: small ? 12 : 18,
-    minHeight: small ? 30 : 50,
-    flexDirection: "row",
-    alignItems: "center",
-    elevation: 3,
-    shadowRadius: 3,
-    shadowOffset: { width: 0, height: 3 },
-    shadowColor: "#111",
-    shadowOpacity: pressHeight.value * 0.25,
-    transform: [{ translateY: -pressHeight.value * 3 }],
-  }));
-  const pressBounceTimeout = useRef<null | ReturnType<typeof setTimeout>>(null);
-  const appendageProps = { color, size: small ? 18 : 24 };
-
-  return (
-    <Pressable
-      style={style}
-      onPress={() => {
-        if (!onPress) return;
-        onPress();
-        pressHeight.value = withSpring(0, { mass: 0.5 });
-        pressBounceTimeout.current = setTimeout(() => {
-          pressHeight.value = withSpring(0.5, { mass: 0.5 });
-          pressBounceTimeout.current = null;
-        }, 250);
-      }}
-      hitSlop={{ left: 6, right: 6, top: 6, bottom: 6 }}
-      onLongPress={onLongPress}
-      disabled={disabled}
-      onResponderEnd={() => {}}
-      onPressIn={() => {
-        pressHeight.value = withSpring(1, { mass: 0.5 });
-      }}
-      onTouchEnd={() => {
-        if (!pressBounceTimeout.current) {
-          pressHeight.value = withSpring(0.5, { mass: 0.5 });
-        }
-      }}
-    >
-      <Animated.View style={containerStyle}>
-        {typeof left === "function" ? left(appendageProps) : left}
-        <Text
-          style={{
-            color,
-            paddingHorizontal: 12,
-            fontSize: 16,
-            fontWeight: primary ? "bold" : "normal",
-            flex: small ? undefined : 1,
-            textAlign,
-          }}
-        >
-          {title}
-        </Text>
-        {typeof right === "function" ? right(appendageProps) : right}
-      </Animated.View>
-    </Pressable>
-  );
+  return <TButton onPress={onPress}>{title}</TButton>;
 }
 
 type AsyncButtonProps = Omit<ComponentProps<typeof Button>, "onPress"> & {
