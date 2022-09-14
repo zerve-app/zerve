e92fa381c6621453637baf5a8ee7cc87d248ddad
Eric Vicenti <eric@vicenti.net>
2022-09-14 02:08:48 +0200

    🤭 blocks unstable-stringify to preserve key order

--- packages/crypto/Blocks.ts
+++ packages/crypto/Blocks.ts
@@ -1,17 +1,13 @@
 import { createHash } from "crypto";
 import { BLOCK_ID_FORMATS, JSONBlock } from "@zerve/zed";
 
-import stringify from "json-stable-stringify";
-
 export async function createJSONBlock(
   value: any,
   formatKey: keyof typeof BLOCK_ID_FORMATS = "z",
 ): Promise<JSONBlock> {
   const format = formatKey || "z";
   const { encoding, algorithm } = BLOCK_ID_FORMATS[format];
-  const jsonValue = stringify(value, {
-    space: 2,
-  });
+  const jsonValue = JSON.stringify(value, null, 2);
 
   const hash = createHash(algorithm)
     .update(jsonValue, "utf8")
