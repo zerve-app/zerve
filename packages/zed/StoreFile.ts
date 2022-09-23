2a3c07b30b8d0953e793e245bd6a16ce8a9e573d
Eric Vicenti <eric@vicenti.net>
2022-09-23 03:25:06 +0200

    😬 auto-capitalize entry names

--- packages/zed/StoreFile.ts
+++ packages/zed/StoreFile.ts
@@ -3,5 +3,9 @@ export function displayStoreFileName(name: string) {
 }
 
 export function prepareStoreFileName(name: string) {
-  return name.replaceAll(" ", "_");
+  let prepared = name.replaceAll(" ", "_");
+  if (prepared[0] !== prepared[0].toUpperCase()) {
+    prepared = `${prepared[0].toUpperCase()}${prepared.slice(1)}`;
+  }
+  return prepared;
 }
