8b5870b428dd34d08eeef5d817dba40c10c5a766
Eric Vicenti <eric@vicenti.net>
2022-09-28 12:43:37 +0200

    ⭐️ icons note/attribution

--- packages/zen-playground/Icons.tsx
+++ packages/zen-playground/Icons.tsx
@@ -3,13 +3,44 @@ import { PageSection } from "@zerve/zen/Page";
 import { HStack } from "@zerve/zen/Stack";
 import { View } from "react-native";
 import { IconValues } from "./IconValues";
-import { Icon } from "../zen/Icon";
-import { useColors } from "../zen/useColors";
+import { Icon } from "@zerve/zen/Icon";
+import { useColors } from "@zerve/zen/useColors";
+import { Paragraph } from "@zerve/zen/Text";
+import { Link } from "@zerve/zen/Link";
 
 export function IconsPlaygroundFeature() {
   const colors = useColors();
   return (
     <FeaturePane title="Icons">
+      <Paragraph>
+        Zen currently utilizes{" "}
+        <Link inline href="https://fontawesome.com/v4/icons/" external>
+          FontAwesome 4
+        </Link>
+        . Imported through{" "}
+        <Link
+          inline
+          href="https://www.npmjs.com/package/@expo/vector-icons"
+          external
+        >
+          @expo/vector-icons
+        </Link>{" "}
+        (with special thanks to{" "}
+        <Link
+          inline
+          href="https://github.com/oblador/react-native-vector-icons"
+          external
+        >
+          react-native-vector-icons
+        </Link>
+        ).
+      </Paragraph>
+      <Paragraph>
+        <Link inline href="https://icons.expo.fyi/" external>
+          Browse all available icons here
+        </Link>
+      </Paragraph>
+
       <PageSection title="Size">
         <HStack padded>
           <Icon name="wifi" size={12} />
